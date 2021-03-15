e99e19f3e6e4ae873afafd9626cba366a03dc4ad
bilpeixoto <bil.peixoto@gmail.com>
2021-03-15 18:37:27 -0300

    Código após aula 1 do curso avançado

---
+++ api/rotas/fornecedores/produtos/index.js
@@ -0,0 +1,15 @@
+const roteador = require('express').Router({ mergeParams: true })
+const Tabela = require('./TabelaProduto')
+
+roteador.get('/', async (req, res) => {
+    const produtos = await Tabela.listar(req.params.idFornecedor)
+    res.send(
+        JSON.stringify(produtos)
+    )
+
+})
+
+const roteadorReclamacoes = require('./reclamacoes')
+roteador.use('/:idProduto/reclamacoes', roteadorReclamacoes)
+
+module.exports = roteador
