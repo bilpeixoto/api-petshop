6a7e5bfd85a6a879a6c5d3ba75c61f9a9761bba6
bilpeixoto <bil.peixoto@gmail.com>
2021-03-17 15:59:13 -0300

    codigo depois da aula 2

--- api/rotas/fornecedores/produtos/TabelaProduto.js
+++ api/rotas/fornecedores/produtos/TabelaProduto.js
@@ -1,9 +1,23 @@
+const { inserir } = require('../TabelaFornecedor')
 const Modelo = require('./ModeloTabelaProduto')
 
 module.exports = {
     listar (idFornecedor) {
         return Modelo.findAll({
             where: {
+                fornecedor: idFornecedor
+            }
+        })
+    },
+
+    inserir (dados) {
+        return Modelo.create(dados)
+    },
+
+    remover (idProduto, idFornecedor) {
+        return Modelo.destroy({
+            where: {
+                id: idProduto,
                 fornecedor: idFornecedor
             }
         })
--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,12 +1,48 @@
 const roteador = require('express').Router({ mergeParams: true })
 const Tabela = require('./TabelaProduto')
+const Produto = require('./Produto')
 
 roteador.get('/', async (req, res) => {
-    const produtos = await Tabela.listar(req.params.idFornecedor)
+    const produtos = await Tabela.listar(req.fornecedor.id)
     res.send(
         JSON.stringify(produtos)
     )
+})
 
+roteador.get('/:idProduto', async (req, res) => {
+    const produtos = await Tabela.listar(req.fornecedor.id)
+    res.send(
+        JSON.stringify(produtos)
+    )
+})
+
+roteador.post('/', async (req, res) => {
+    try{
+        const idFornecedor = req.fornecedor.id
+        const corpo = req.body
+        dados = Object.assign({}, corpo, {fornecedor: idFornecedor})
+        const produto = new Produto(dados)
+        await produto.criar()
+        res.status(201).json(produto)
+    } catch(erro) {
+        res.send(JSON.stringify(erro))
+    }
+})
+
+roteador.delete('/:id', async (req, res) => {
+    try {
+        dados = {
+            id: req.params.id,
+            fornecedor: req.fornecedor.id
+        }
+        const produto = new Produto(dados)
+        await produto.apagar()
+        res.status(204)
+        res.end()
+    } catch (erro) {
+        res.status(400)
+        res.send(JSON.stringify(erro))
+    }
 })
 
 const roteadorReclamacoes = require('./reclamacoes')
